a92cba3c141727d6856081a7d3c018c9cb1cb1c0
andres-vidal <andresvidalberriel@gmail.com>
2020-06-15 23:46:06 -0300

    Remove unnecesary session store elements.

--- src/store/session/reducer.js
+++ src/store/session/reducer.js
@@ -11,18 +11,9 @@ export default function SessionReducer(state = initialState, action) {
     const { type, payload } = action;
 
     switch (type) {
-        case actionTypes.SET_SESSION_LOADING: {
-            return { ...initialState, loading: true, error: undefined };
-        }
-
         case actionTypes.SET_SESSION_SUCCESS: {
             const { user, token } = payload;
             return { ...initialState, user, token };
-        }
-
-        case actionTypes.SET_SESSION_ERROR: {
-            const { error } = payload;
-            return { ...initialState, error };
         }
 
         case actionTypes.RESET_SESSION_STATE: {
--- src/store/session/sagas.js
+++ src/store/session/sagas.js
@@ -6,35 +6,6 @@ import SessionController from "api/controllers/session";
 import { UNAUTHORIZED } from "utils/http-status";
 import { setAuthenticationToken } from "api/axios";
 
-function* signIn(action) {
-    const { email, authenticationString } = action.payload;
-
-    try {
-        yield put(actions.setSessionLoading());
-        const response = yield SessionController.signIn(
-            email,
-            authenticationString
-        );
-
-        const user = response.data;
-        const token = response.headers["rb-authorization"];
-
-        yield put(actions.setSessionSuccess(user, token));
-    } catch (error) {
-        switch (error.response.status) {
-            case UNAUTHORIZED: {
-                error.message = "error:incorrectEmailOrPassword";
-            }
-        }
-
-        yield put(actions.setSessionError(error));
-    }
-}
-
-function* watchSignIn() {
-    yield takeEvery(actionTypes.SIGN_IN_REQUEST, signIn);
-}
-
 function* updateSession() {
     const { user, token } = yield select(state => state.session);
 
@@ -58,5 +29,5 @@ function* watchUpdateSession() {
 }
 
 export default function* rootSaga() {
-    yield all([watchSignIn(), watchUpdateSession()]);
+    yield all([watchUpdateSession()]);
 }
